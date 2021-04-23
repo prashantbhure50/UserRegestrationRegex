2b0565e9703706bf6ed553266b1597fb83bb1b66
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-24 00:13:22 +0530

    file is updated as per UC3

--- UserRegestrationRegex/Pattern.cs
+++ UserRegestrationRegex/Pattern.cs
@@ -9,9 +9,14 @@ namespace UserRegestrationRegex
     {
 
         string regex = "^[A-Z][a-zA-Z]{2,}$";
+        string emailregex = "^[a-zA-Z]{3}[.]([a-zA-z]{3})[@][a-zA-Z]{2}[.][a-zA-Z]{2}[.]([a-zA-Z]{2})";
         public bool Check(string input)
         {
             return Regex.IsMatch(input, regex);
+        }
+        public bool EmailCheck(string input)
+        {
+            return Regex.IsMatch(input, emailregex);
         }
     }
 }
--- UserRegestrationRegex/Program.cs
+++ UserRegestrationRegex/Program.cs
@@ -12,6 +12,8 @@ namespace UserRegestrationRegex
             Console.WriteLine("FirstName " + firstName);
             bool lastName = pattern.Check("Prashant");
             Console.WriteLine("LastName " + lastName);
+            bool email = pattern.EmailCheck("abc.xyz@bl.co.in");
+            Console.WriteLine("Email " + email);
         }
     }
 }
